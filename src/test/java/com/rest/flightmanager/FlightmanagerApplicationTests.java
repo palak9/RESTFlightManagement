db1222f46820075ac8afd7dfa9c24aa9282eb8e8
Palak Rathi <palakrathi09@gmail.com>
2023-07-13 21:16:48 +0530

    first commit

---
+++ src/test/java/com/rest/flightmanager/FlightmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.flightmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
